79ba655eeea52b47f7d4bb0008c847d47902d11d
Aymane-Kabil <73406336+Aymane-Kabil@users.noreply.github.com>
2025-06-04 15:27:01 -0400

    update

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 
-url ='http://localhost:8000/test'
+#url ='http://localhost:8000/test'
+url = 'https://rcw1003-instant1-g2bpfaena8gjbdfs.canadaeast-01.azurewebsites.net/'
 response =requests.get(url)
 response = response.json()
 print(response['message'])
